6988e6099a033210715f777dd40a87068ac07f90
Neill Cox <neill@ingenious.com.au>
2015-06-23 17:02:44 +1000

    Packaging fixes

--- setup.py
+++ setup.py
@@ -10,11 +10,12 @@ from setuptools import setup, find_packages
 from codecs import open
 from os import path
 
-here = path.abspath(path.dirname(__file__))
+def read(*parts):
+    filename = path.join(path.dirname(__file__), *parts)
+    with open(filename, encoding='utf-8') as fp:
+        return fp.read()
 
-# Get the long description from the relevant file
-with open(path.join(here, 'DESCRIPTION.rst'), encoding='utf-8') as f:
-    long_description = f.read()
+
 
 setup(
     name='angular-gettext-babel',
@@ -28,7 +29,7 @@ setup(
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
-        'Topic :: Software Development :: Internationalisation',
+        'Topic :: Software Development :: Internationalization',
         'License :: OSI Approved :: Apache Software License',
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
